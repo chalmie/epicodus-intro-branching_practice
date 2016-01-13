906e8c9a248d50ee9670d21497c0010c7fd0ce31
Blair Peterson and Joseph Karasek <blairchalmers@gmail.com>
2016-01-13 09:13:15 -0800

    add function to three other buttons

--- js/scripts.js
+++ js/scripts.js
@@ -13,11 +13,19 @@ $(document).ready(function() {
   });
   // turtle button
   $('button#turtleBtn').click(function() {
-    alert('Your turtle button works!');
+    // alert('Your turtle button works!');
+    // add sibling classes hidden
+    $('.turtle').siblings().addClass('hidden');
+    // remove class hidden from chosen animal
+    $('.turtle').removeClass('hidden');
   });
   // insecta button
   $('button#insectsBtn').click(function() {
-    alert('Your insects button works!');
+    // alert('Your insects button works!');
+    // add sibling classes hidden
+    $('.insects').siblings().addClass('hidden');
+    // remove class hidden from chosen animal
+    $('.insects').removeClass('hidden');
   });
 
 });
