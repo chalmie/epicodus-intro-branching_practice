1689560a494c3d365d19814e9d907b1144583ba1
Blair Peterson and Joseph Karasek <blairchalmers@gmail.com>
2016-01-13 08:58:50 -0800

    connect buttons to basic scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+  // functionality to buttons
+  // on click show given animal content and hide other content
+  $('button').click(function() {
+    alert('Your button works!');
+  });
+
+});
