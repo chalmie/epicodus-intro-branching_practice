792fbaae76a84c072402a9399ac7a0d6033fce94
Blair Peterson and Joseph Karasek <joseph.karasek@gmail.com>
2016-01-13 09:02:56 -0800

    add and connect unique ids for each button

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,17 @@ $(document).ready(function() {
 
   // functionality to buttons
   // on click show given animal content and hide other content
-  $('button').click(function() {
-    alert('Your button works!');
+  // snake button
+  $('button#snakeBtn').click(function() {
+    alert('Your snake button works!');
+  });
+  // turtle button
+  $('button#turtleBtn').click(function() {
+    alert('Your turtle button works!');
+  });
+  // insecta button
+  $('button#insectsBtn').click(function() {
+    alert('Your insects button works!');
   });
 
 });
